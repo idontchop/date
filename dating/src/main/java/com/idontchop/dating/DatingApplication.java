16d1019a6f0087462c35d40ff4402722da172f8e
root <root@nate-ubu>
2019-09-22 12:58:14 -0700

    JPA Entity Bug

--- dating/src/main/java/com/idontchop/dating/DatingApplication.java
+++ dating/src/main/java/com/idontchop/dating/DatingApplication.java
@@ -1,14 +1,36 @@
 package com.idontchop.dating;
 
+import service.SecurityConfigDating;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import datingEntities.Gender;
+import datingEntities.User;
+
+
+
+@SpringBootApplication(scanBasePackages = {"com.idontchop"})
 @RestController
+@EnableAutoConfiguration
+@ComponentScan("service")
 public class DatingApplication {
 
+	@Autowired
+	private SecurityConfigDating c;
+	
+
+	@Autowired
+	private Gender g;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DatingApplication.class, args);
 	}
